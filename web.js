de301a5a0456c72145a14c8abee4b6506d16d062
Aitor Oses <aitor.oses@gmail.com>
2013-06-03 05:57:17 +0200

    jade stylus

--- web.js
+++ web.js
@@ -1,10 +1,68 @@
-var express = require("express");
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
 var app = express();
-app.use(express.logger());
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
+app.configure(function() {
+	app.use(express.static(__dirname + '/public'))
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.session({ secret: 'keyboard cat' }));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
+	app.set('views', __dirname + '/views')
+	app.set("view engine", "jade");
+	app.use(stylus.middleware(
+	  { src: __dirname + '/public'
+	  , compile: compile
+	  }
+	))
+	app.use(express.logger());
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('index', { title : 'Home' });
 });
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
